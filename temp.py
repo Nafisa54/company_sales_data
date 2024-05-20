bcb470a0139410b6a432d6a18429a77f35db0c2c
Nafisa54 <nafisaisahtr@gmail.com>
2024-05-20 18:46:48 +0100

    my_assignment

---
+++ temp.py
@@ -0,0 +1,7 @@
+#import libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pylab as plt
+
+#import dataset
+IPV=pd.read_excel("C:/Users/ADDIS/Desktop/PYTHON BEINNER CLASS/IPV Dataset.xlsx")
